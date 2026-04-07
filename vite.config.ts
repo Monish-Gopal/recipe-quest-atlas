62a68176c353a78f6db9476ada5977a9a70ca5ef
Monish-Gopal <68846573+Monish-Gopal@users.noreply.github.com>
2026-04-07 19:03:37 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/recipe-quest-atlas/",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
